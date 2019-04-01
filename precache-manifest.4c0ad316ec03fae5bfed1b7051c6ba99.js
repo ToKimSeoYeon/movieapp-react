14b1ec76a957e12a5420293e2ae8049ac280d754
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-04-01 19:27:22 +0900

    Updates

---
+++ precache-manifest.4c0ad316ec03fae5bfed1b7051c6ba99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92e5fc8c6a4ae4f953ba",
+    "url": "/movieapp-react/static/js/runtime~main.01389d41.js"
+  },
+  {
+    "revision": "92a7b0a6a3b28a48d8f9",
+    "url": "/movieapp-react/static/js/main.5f041e9b.chunk.js"
+  },
+  {
+    "revision": "026da97c9cf2fe34d3df",
+    "url": "/movieapp-react/static/js/2.0618d762.chunk.js"
+  },
+  {
+    "revision": "92a7b0a6a3b28a48d8f9",
+    "url": "/movieapp-react/static/css/main.501068ab.chunk.css"
+  },
+  {
+    "revision": "db9819076492acaeffc4c795da3f8e74",
+    "url": "/movieapp-react/index.html"
+  }
+];
